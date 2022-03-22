f97d9837980179716f0a42aee91df771294d4fa5
Ria <62290817+prshah101@users.noreply.github.com>
2022-03-23 10:01:36 +1100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", function(res,res){
+res.send("Working")
+})
+
+app.listen(process.env.PORT || port, () => { //listen to whatever port is avilable on heroku
+    console.log(`Example app listening on port ${port}`)
+  })
